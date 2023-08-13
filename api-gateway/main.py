5a33988647394b00bb1d4538674f1d0655e44f04
Anshul Kanwar <me@anshulkanwar.com>
2023-08-13 10:34:54 +0000

    add CORS middleware

--- api-gateway/main.py
+++ api-gateway/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 from routers import layout_analysis, logo_redaction
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     layout_analysis.router,
